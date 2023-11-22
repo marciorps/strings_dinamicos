823a8c0c90199a134bbde814902422bacd0103bb
marciorps <marciorps@yahoo.com,.br>
2023-11-22 10:57:04 -0300

    strins dinamicos

---
+++ strings_dinamicos.py
@@ -0,0 +1,25 @@
+# Strins dinamicos
+
+nome = 'Márcio'
+email = 'marcio@yahoo.com.br'
+
+# Olá Márcio, você cadastrou o email marcio@yahoo.com.br, essa informação está correta?
+print(f'Olá {nome}, você cadastrou o email {email}, essa informação está correta?')
+
+#Desafio 1 
+## Crie os seuinte string dinâmicos
+nome = 'carol'
+hobby = 'ouvir Música'
+print(f"ola sou a {nome} e gosto de {hobby}")
+
+b = 'ba'
+parte2 = 'nica'
+a = 'a'
+r = 'ri'
+parte1 = 'eletrô'
+t = 'te'
+# resultado
+'bateria eletrônica'
+
+print(f'{b}{t}{r}{a} {parte1}{parte2}')
+
